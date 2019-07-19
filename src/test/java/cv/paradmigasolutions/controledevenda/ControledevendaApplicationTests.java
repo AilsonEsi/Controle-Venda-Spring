8e0520f59c8a16ddf5c6eb0e7840df02f4f78970
Ailson Moreira <ailsonesi96@gmail.com>
2019-07-19 21:04:36 -0100

    Initialize commit

---
+++ src/test/java/cv/paradmigasolutions/controledevenda/ControledevendaApplicationTests.java
@@ -0,0 +1,16 @@
+package cv.paradmigasolutions.controledevenda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControledevendaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
