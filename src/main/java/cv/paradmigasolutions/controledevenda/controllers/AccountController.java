f007b62b41d8485ae698a94a57a73e844fc1297b
Ailson Moreira <ailsonesi96@gmail.com>
2019-07-20 01:41:28 -0100

    Update layout and set configurations

---
+++ src/main/java/cv/paradmigasolutions/controledevenda/controllers/AccountController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cv.paradmigasolutions.controledevenda.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author programmer
+ */
+@Controller
+public class AccountController {
+    
+    @GetMapping(value = "/login")
+    public String login(){
+        
+        return "account/login";
+    }
+    
+    @GetMapping(value = "/registar")
+    public String signUp(){
+        
+        return "account/signup";
+    }
+}
