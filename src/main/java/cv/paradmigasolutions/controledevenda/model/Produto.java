46293937d623adc1cdb0f099949ceff2ce6dbd22
Ailson Moreira <ailsonesi96@gmail.com>
2019-08-19 17:04:10 -0100

    ch: uncomment remover modal script

---
+++ src/main/java/cv/paradmigasolutions/controledevenda/model/Produto.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cv.paradmigasolutions.controledevenda.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author programmer
+ */
+@Entity(name = "pdt_produto")
+public class Produto extends AudityModel<String> {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "pdt_nome", nullable = false, unique = true)
+    @NotNull(message = "Ob")
+    private String nome;
+    private String desc;
+    private Float precoCompra;
+    private Float precoVenda;
+    private String grupo;
+    private String subGrupo;
+    private String imgURL;
+    private String obs;
+    private Integer quantidade;
+    private Fornecedor fornecedor;
+    private String fabricante;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Float getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(Float precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public Float getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(Float precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getSubGrupo() {
+        return subGrupo;
+    }
+
+    public void setSubGrupo(String subGrupo) {
+        this.subGrupo = subGrupo;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
+    public void setObs(String obs) {
+        this.obs = obs;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+    
+    
+}
